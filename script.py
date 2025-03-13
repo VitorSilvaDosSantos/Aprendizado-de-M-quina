dcf7a500493d67ecd43d626131698838c81a8930
VitorSilvaDosSantos <vitor.s.santos@iesb.edu.br>
2025-03-12 21:44:45 -0300

    Aula dia 12/03

--- script.py
+++ script.py
@@ -1,1 +1,35 @@
-print("hello world")
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("./marketing.csv", delimiter=",")
+
+print(df.head(5))
+
+print(df.describe(include="all"))
+
+print(df.info())
+
+print(df['converted'].head(5))
+
+print(df['converted'].dtype)
+
+(df['converted']) = print(df['converted'].astype('bool'))
+
+print(df)
+
+np.where(df['marketing_channel'] == 'House Ads', True, False)
+
+channel_dict = {
+    'House Ads': 1,
+    'Instagran' : 2,
+    'Facebook': 3,
+    'Email': 4,
+    'Push': 5,
+    '': 0
+}
+
+print(df['marketing_channel'].map(channel_dict))
+
+
+
+
